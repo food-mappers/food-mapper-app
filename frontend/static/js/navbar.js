ad6a1720eede11cd7f78691e8f121cdc7e4e1837
Andy <adpruett@gmail.com>
2013-11-06 20:05:14 -0500

    missed a file

---
+++ frontend/static/js/navbar.js
@@ -0,0 +1,9 @@
+// Javascript used in the navbar.
+
+// Dynamically populate the Google Docs Feedback Form URL
+var feedbackFormUrl = 'https://docs.google.com/forms/d/15LLhT_SJurl4qr_mpcjqhdw0UcfFTyOsYKowtdBv9PM/viewform';
+$('#feedback-link').attr('href', function() { return feedbackFormUrl; });
+
+// Alternate version doesn't work so hot... additional fields in the document URL break the google forms parse.
+$('#feedback-link').attr('href', function() { return feedbackFormUrl + '&entry_0=' + window.document.location.host + window.document.location.pathname;});
+
